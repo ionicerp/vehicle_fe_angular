4a6476dbf4a3c7d1eb1fc4425ec849b7e9678503
Vincent Tan <vincenttan83@live.com>
2023-07-14 18:20:18 +0800

    added log output to debug

--- start.sh
+++ start.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+echo "Starting the application..."
+
 # Replace placeholder values in JavaScript files with environment variables
 sed -i -e 's|API_URL|'"$API_URL"'|g' /usr/share/nginx/html/vehicle/main*.js
 sed -i -e 's|ENVIRONMENT_NAME|'"$ENVIRONMENT_NAME"'|g' /usr/share/nginx/html/vehicle/main*.js
 
 # Start NGINX
 nginx -g 'daemon off;'
+
+echo "Application startup script finished."
+
