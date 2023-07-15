436e0e827ddf07a547d1c6c11c40b93212508b11
Vincent Tan <vincenttan83@live.com>
2023-07-15 13:42:29 +0800

    show of app metadata

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,7 +32,7 @@ export class AppComponent implements OnInit {
     // const decodedToken = decodeURIComponent(escape(window.atob(base64)));
     // console.log(decodedToken);
     // const oriToken = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InVqV0ZwbFp6emw1dmVvUEZmQ3REbCJ9.eyJodHRwczovL2FwcC5pb25pY2VycC5jb20vYXBwX21ldGFkYXRhIjp7ImNvbXBhbnlJZHMiOlsiYXV0b2xpdmUiLCJpb25pY2VycCJdfSwiaXNzIjoiaHR0cHM6Ly9pb25pY2VycC51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjRhMjY1YjcxM2ExNTg1MjhiOTBlMGVlIiwiYXVkIjpbImh0dHBzOi8vaW9uaWNlcnAudXMuYXV0aDAuY29tL2FwaS92Mi8iLCJodHRwczovL2lvbmljZXJwLnVzLmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2ODgzODI5MzYsImV4cCI6MTY4ODQ2OTMzNiwiYXpwIjoidFlrMGpEd2QyOU9HOVY4Qm1aSmwzRm9YYVJyNXk4R08iLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIn0.bs3tQ0V5K_itYmUHP8ROvz3iWljqLmmiMq_HrpJIeL2IQ7jCChu7WrrZCYt9GomK_lUZ2WbfEdqIntfCV5UYg4kGYL2ehQT0JcLwO3k0e84OrI1UGsNUCE6J2SOrENm6mQjNidTSqrAOVtbEDVmrUTHiyaSdCrkOthI3pA1Y2-vtrs3kiY0keFjIVxZcST4HF3tizu7b1VMrXe0nbShyTINcQGDesSGCjQcc4oo5901qyMjvG1IUo5nX_R6gAoGR13oCotGc-iEzKGzbgJp52-gcwKwMP39Cinyk2-1rqAqJ2XEPTa5mLL6zvpn5cw4TNeX8BojN0X-tNswwpo8h_g';
-    
+
     // console.log(jwtDecodeToken);
 
 
@@ -51,9 +51,12 @@ export class AppComponent implements OnInit {
     // console.log(tokenWithAud);
     const jwtDecodeToken: any = jwtDecode(tokenWithAud);
     // const userAppMetadata = jwtDecodeToken.user_app_metadata;
-    
+
     console.log('jwtDecodeToken');
     console.log(jwtDecodeToken)
+
+    console.log('app_metadata');
+    console.log(jwtDecodeToken['https://app.ionicerp.com/app_metadata'])
 
     const vehicle = await firstValueFrom(this.vehicleService.get(tokenWithAud ?? ''));
     console.log('vehicle');
