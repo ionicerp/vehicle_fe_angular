ce8e0210fbac53e1ab76b0d1e47ccc844601121d
Vincent Tan <vincenttan83@live.com>
2023-07-15 13:24:15 +0800

    added log out of user app meta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,8 +32,7 @@ export class AppComponent implements OnInit {
     // const decodedToken = decodeURIComponent(escape(window.atob(base64)));
     // console.log(decodedToken);
     // const oriToken = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InVqV0ZwbFp6emw1dmVvUEZmQ3REbCJ9.eyJodHRwczovL2FwcC5pb25pY2VycC5jb20vYXBwX21ldGFkYXRhIjp7ImNvbXBhbnlJZHMiOlsiYXV0b2xpdmUiLCJpb25pY2VycCJdfSwiaXNzIjoiaHR0cHM6Ly9pb25pY2VycC51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjRhMjY1YjcxM2ExNTg1MjhiOTBlMGVlIiwiYXVkIjpbImh0dHBzOi8vaW9uaWNlcnAudXMuYXV0aDAuY29tL2FwaS92Mi8iLCJodHRwczovL2lvbmljZXJwLnVzLmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2ODgzODI5MzYsImV4cCI6MTY4ODQ2OTMzNiwiYXpwIjoidFlrMGpEd2QyOU9HOVY4Qm1aSmwzRm9YYVJyNXk4R08iLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIn0.bs3tQ0V5K_itYmUHP8ROvz3iWljqLmmiMq_HrpJIeL2IQ7jCChu7WrrZCYt9GomK_lUZ2WbfEdqIntfCV5UYg4kGYL2ehQT0JcLwO3k0e84OrI1UGsNUCE6J2SOrENm6mQjNidTSqrAOVtbEDVmrUTHiyaSdCrkOthI3pA1Y2-vtrs3kiY0keFjIVxZcST4HF3tizu7b1VMrXe0nbShyTINcQGDesSGCjQcc4oo5901qyMjvG1IUo5nX_R6gAoGR13oCotGc-iEzKGzbgJp52-gcwKwMP39Cinyk2-1rqAqJ2XEPTa5mLL6zvpn5cw4TNeX8BojN0X-tNswwpo8h_g';
-    // const jwtDecodeToken: any = jwtDecode(oriToken);
-    // const userAppMetadata = jwtDecodeToken.user_app_metadata;
+    
     // console.log(jwtDecodeToken);
 
 
@@ -49,10 +48,15 @@ export class AppComponent implements OnInit {
     // const token = await firstValueFrom(this.authenticateService.getAccessTokenSilently());
     // console.log(token);
     const tokenWithAud = await firstValueFrom(this.authenticateService.getAccessTokenSilentlyWithAudience());
-    console.log(tokenWithAud);
+    // console.log(tokenWithAud);
+    const jwtDecodeToken: any = jwtDecode(tokenWithAud);
+    const userAppMetadata = jwtDecodeToken.user_app_metadata;
+    
+    console.log('user_app_metadata');
+    console.log(userAppMetadata)
 
     const vehicle = await firstValueFrom(this.vehicleService.get(tokenWithAud ?? ''));
-
+    console.log('vehicle');
     console.log(vehicle);
 
     // const decodedToken = atob(token);
