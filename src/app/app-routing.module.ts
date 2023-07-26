9f83cb856a2096aa425e6559fc2787c85483ede9
Vincent Tan <vincenttan83@live.com>
2023-07-26 23:33:55 +0800

    added eater module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrandModule } from './modules/brand/brand.module';
+import { ModelModule } from './modules/model/model.module';
 
-const routes: Routes = [{ path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) }, { path: 'model', loadChildren: () => import('./modules/model/model.module').then(m => m.ModelModule) }, { path: 'brand', loadChildren: () => import('./modules/brand/brand.module').then(m => m.BrandModule) }];
+const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) },
+  { path: 'model', loadChildren: () => import('./modules/model/model.module').then(m => m.ModelModule) },
+  { path: 'brand', loadChildren: () => import('./modules/brand/brand.module').then(m => m.BrandModule) },
+  { path: 'eagerbrand', component: BrandModule },
+  { path: 'eagermodel', component: ModelModule },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
